976f6ae6373c717735dbe58c9ccd74e5a317a4cd
Soumyadip Roy <roysoumyadip334@gmail.com>
2024-07-20 21:06:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
